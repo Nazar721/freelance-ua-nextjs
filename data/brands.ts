15bad5465f7b8ee0f669570d4a73ada0c29e850c
Nazar721 <nazarmarchak2000@gmail.com>
2026-05-10 00:26:33 +0300

    add 4 services

--- data/brands.ts
+++ data/brands.ts
@@ -1,13 +1,21 @@
 import { Brand } from "@/types";
 
 export const brands: Brand[] = [
-  { name: "DJANOVA", logo: "/media/brands/yt_banner_djanova.jpg" },
-  { name: "Luciol", logo: "/media/brands/luciol.jpg" },
-  { name: "NordLead", logo: "/media/brands/nordleadf.jpg" },
-  { name: "TuneAppetite", logo: "/media/brands/tuneapperite.jpg" },
-  { name: "Vse Hub", logo: "/media/brands/vse_hub.jpg" },
-  { name: "Yednist Digital", logo: "/media/brands/yednistdigital.webp" },
-  { name: "Південь Солар", logo: "/media/brands/pivdensolar.webp" },
-  { name: "AutoAmerica", logo: "/media/brands/tiktok_banner_autoamerica.jpg" },
-  { name: "Sheremet ETH", logo: "/media/brands/inst_banner_sheremet_eth.jpg" },
-];
+	{ name: 'DJANOVA', logo: '/media/brands/yt_banner_djanova.jpg' },
+	{ name: 'Luciol', logo: '/media/brands/luciol.jpg' },
+	{ name: 'NordLead', logo: '/media/brands/nordleadf.jpg' },
+	{ name: 'TuneAppetite', logo: '/media/brands/tuneapperite.jpg' },
+	{ name: 'Vse Hub', logo: '/media/brands/vse_hub.jpg' },
+	{ name: 'Yednist Digital', logo: '/media/brands/yednistdigital.webp' },
+	{ name: 'Південь Солар', logo: '/media/brands/pivdensolar.webp' },
+	{ name: 'AutoAmerica', logo: '/media/brands/tiktok_banner_autoamerica.jpg' },
+	{ name: 'Sheremet ETH', logo: '/media/brands/inst_banner_sheremet_eth.jpg' },
+	{
+		name: 'Repair Collision PL (kolizjaoc.pl)',
+		logo: '/media/brands/repair_collision.jpg',
+	},
+	{
+		name: 'Sestro – beauty studio',
+		logo: '/media/brands/sestro.jpg',
+	},
+]
